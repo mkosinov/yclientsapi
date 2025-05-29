7b435d058f4bef8fda12306ea1f4814aae276310
Maksim Kosinov <mkosinov@mail.ru>
2025-05-30 00:42:53 +0300

    Implement CRUD operations for service categories, including create, update, and delete methods. Update schemas for request and response validation. Add integration tests and fixtures for service category operations. Update version to 0.1.6 and enhance README with test coverage details.

--- src/yclientsapi/components/service_category.py
+++ src/yclientsapi/components/service_category.py
@@ -1,10 +1,14 @@
+from dataclasses import asdict
 from http import HTTPMethod
 
 import orjson
 
 from yclientsapi.schema.service_category import (
+    ServiceCategoryCreateRequest,
+    ServiceCategoryCreateResponse,
+    ServiceCategoryGetResponse,
     ServiceCategoryListResponse,
-    ServiceCategoryResponse,
+    ServiceCategoryUpdateRequest,
 )
 
 
@@ -32,19 +36,74 @@ class ServiceCategory:
     def get(
         self,
         category_id: str | int,
-    ) -> ServiceCategoryResponse:
-        """Returns service category.
+    ) -> ServiceCategoryGetResponse:
+        """Return a single service category by its ID.
 
-        :param category_id: id of service category
-        :return: ServiceCategoryResponse
+        :param category_id: ID of the service category
+        :return: ServiceCategoryGetResponse
         """
-        url_suffix = (
-            "/v1/company/{company_id}/service_categories/{category_id}"
-        )
+        url_suffix = "/v1/service_category/{company_id}/{category_id}"
         response = self.__api._sender.send(
             method=HTTPMethod.GET,
             url_suffix=url_suffix,
             url_params={"category_id": category_id},
             headers=self.__api._headers.base_with_user_token,
         )
-        return ServiceCategoryResponse(**orjson.loads(response.content))
+        return ServiceCategoryGetResponse(**orjson.loads(response.content))
+
+    def create(
+        self,
+        service_category: ServiceCategoryCreateRequest,
+    ) -> ServiceCategoryCreateResponse:
+        """Create a new service category.
+
+        :param service_category: Data for the new service category
+        :return: ServiceCategoryCreateResponse
+        """
+        url_suffix = "/v1/service_categories/{company_id}"
+        payload = asdict(service_category)
+        response = self.__api._sender.send(
+            method=HTTPMethod.POST,
+            url_suffix=url_suffix,
+            headers=self.__api._headers.base_with_user_token,
+            json=payload,
+        )
+        return ServiceCategoryCreateResponse(**orjson.loads(response.content))
+
+    def update(
+        self,
+        category_id: str | int,
+        service_category: ServiceCategoryUpdateRequest,
+    ) -> ServiceCategoryGetResponse:
+        """Update an existing service category by its ID.
+
+        :param category_id: ID of the service category to update
+        :param service_category: Updated data for the service category
+        :return: ServiceCategoryGetResponse
+        """
+        url_suffix = "/v1/service_category/{company_id}/{category_id}"
+        payload = asdict(service_category)
+        response = self.__api._sender.send(
+            method=HTTPMethod.PUT,
+            url_suffix=url_suffix,
+            url_params={"category_id": category_id},
+            headers=self.__api._headers.base_with_user_token,
+            json=payload,
+        )
+        return ServiceCategoryGetResponse(**orjson.loads(response.content))
+
+    def delete(
+        self,
+        category_id: str | int,
+    ) -> None:
+        """Delete a service category by its ID.
+
+        :param category_id: ID of the service category to delete
+        """
+        url_suffix = "/v1/service_category/{company_id}/{category_id}"
+        self.__api._sender.send(
+            method=HTTPMethod.DELETE,
+            url_suffix=url_suffix,
+            url_params={"category_id": category_id},
+            headers=self.__api._headers.base_with_user_token,
+        )
