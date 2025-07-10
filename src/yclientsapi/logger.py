1dd0048a792b66d4941731e5dbaf906592959638
Maksim Kosinov <mkosinov@mail.ru>
2025-07-10 20:43:44 +0300

    fix(logger): add type ignore comment for Callable in log_call decorator to maintain compatibility with type checkers

--- src/yclientsapi/logger.py
+++ src/yclientsapi/logger.py
@@ -6,7 +6,7 @@ from typing import TypeVar, cast
 F = TypeVar("F", bound=Callable)
 
 
-def log_call(func: F) -> F:
+def log_call(func: F) -> F:  # type: ignore
     """
     Decorator to log the calling of API methods using self.__api.logger.
     Logs method name and arguments (excluding self).
